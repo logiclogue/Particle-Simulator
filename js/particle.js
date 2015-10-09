509b5ffbcd772d71c9048f5cfd058e38a6a3c1dc
logiclogue <jordanpaullord@hotmail.co.uk>
2015-10-09 19:52:34 +0100

    Progress on combining momentums

--- js/particle.js
+++ js/particle.js
@@ -1,5 +1,5 @@
 var Particle = (function () {
-	function self(_x, _y) {
+	function Particle(_x, _y) {
 		this.x = _x;
 		this.y = _y;
 		this.angle = 0;
@@ -12,17 +12,35 @@ var Particle = (function () {
 	}
 
 
-	function addVelocity(self, speed, direction) {
-		var o1 = Math.sin(self.angle) * self.speed;
-		var a1 = Math.cos(self.angle) * self.speed;
-		var o2 = Math.sin(direction) * speed;
-		var a2 = Math.cos(direction) * speed;
+	function calcSpeedAngle(speed_1, angle_1, speed_2, angle_2) {
+		var o1 = Math.sin(angle_1) * speed_1;
+		var a1 = Math.cos(angle_1) * speed_1;
+		var o2 = Math.sin(angle_2) * speed_2;
+		var a2 = Math.cos(angle_2) * speed_2;
 		var opp = o1 + o2;
 		var adj = a1 + a2;
 
-		self.angle = Math.atan2(opp, adj);
-		self.speed = Math.sqrt(Math.pow(opp, 2) + Math.pow(adj, 2));
-	};
+		return {
+			angle: Math.atan2(opp, adj),
+			speed: Math.sqrt(Math.pow(opp, 2) + Math.pow(adj, 2))
+		};
+	}
+
+	function addVelocity(self, speed, direction) {
+		var speedAngle = calcSpeedAngle(self.speed, self.angle, speed, direction);
+
+		self.angle = speedAngle.angle;
+		self.speed = speedAngle.speed;
+	}
+
+	function addMomentum(self, speed, mass, direction) {
+		var speedAngle = calcSpeedAngle(self.speed * self.mass, self.angle, speed * mass, direction);
+
+		self.angle = speedAngle.angle;
+		self.speed = speedAngle.speed / (self.mass + mass);
+	}
+
+	console.log(calcSpeedAngle(10, 0, 20, Math.PI));
 
 	function checkCollision(self) {
 		for (var i = 0, max = Universe.particles.length; i < max; i += 1) {
@@ -30,6 +48,7 @@ var Particle = (function () {
 			// if the particle is colliding
 			if (Math.sqrt(Math.pow(Universe.particles[i].y - self.y, 2) + Math.pow(Universe.particles[i].x - self.x, 2)) < Math.sqrt(Universe.particles[i].mass + self.mass) && self.mass > Universe.particles[i].mass && Universe.particles[i] !== self) {
 				self.mass += Universe.particles[i].mass;
+				//addMomentum(this, Universe.particles[i].speed, Universe.particles[i].mass, Universe.particles[i].direction);
 				self.speed = 0;
 				Universe.particles[i].destroy();
 
@@ -38,7 +57,7 @@ var Particle = (function () {
 				max -= 1;
 			}
 		}
-	};
+	}
 
 	function updatePos(self) {
 		self.x += Math.sin(self.angle) * self.speed;
@@ -49,10 +68,10 @@ var Particle = (function () {
 			pos.x = self.x;
 			pos.y = self.y;
 		}
-	};
+	}
 	
 
-	self.prototype.update = function () {
+	Particle.prototype.update = function () {
 		updatePos(this);
 		checkCollision(this);
 
@@ -69,7 +88,7 @@ var Particle = (function () {
 		}
 	};
 
-	self.prototype.focus = function () {
+	Particle.prototype.focus = function () {
 		for (var i = 0, max = Universe.particles.length; i < max; i += 1) {
 			Universe.particles[i].isFocus = false;
 		}
@@ -77,10 +96,10 @@ var Particle = (function () {
 		this.isFocus = true;
 	};
 
-	self.prototype.destroy = function () {
+	Particle.prototype.destroy = function () {
 		Universe.particles.splice(Universe.particles.indexOf(this), 1);
 	};
 
 
-	return self;
+	return Particle;
 }());
--- js/universe.js
+++ js/universe.js
@@ -19,20 +19,26 @@ var Universe = (function (self) {
 	function updateParticles() {
 		// update all particles and draw them
 		for (var i = 0, max = self.particles.length; i < max; i += 1) {
-			try {
+			if (self.particles[i] !== undefined) {
 				self.particles[i].update();
-
-				Main.ctx.beginPath();
-				Main.ctx.arc(self.particles[i].x/* - pos.x + (Main.c.width / 2)*/, self.particles[i].y/* - pos.y + (Main.c.height / 2)*/, Math.sqrt(self.particles[i].mass), 0, 2 * Math.PI);
-				Main.ctx.fill();
 			}
-			catch (e) {}
+			// may remove later!
+			else {
+				i -= 1;
+				max -= 1;
+			}
+		}
+
+		for (var i = 0, max = self.particles.length; i < max; i += 1) {
+			Main.ctx.beginPath();
+			Main.ctx.arc(self.particles[i].x/* - pos.x + (Main.c.width / 2)*/, self.particles[i].y/* - pos.y + (Main.c.height / 2)*/, Math.sqrt(self.particles[i].mass), 0, 2 * Math.PI);
+			Main.ctx.fill();
 		}
 	}
 
 
 	self.start = function () {
-		inverval = setInterval(mainLoop, 1000/60);
+		inverval = setInterval(mainLoop, 1000 / 60);
 	};
 
 
