bb4dc728d944b33519033ae48c98e39ef7d1eec4
logiclogue <jordanpaullord@hotmail.co.uk>
2015-10-27 20:46:53 +0000

    Update particle gravity loop

--- js/particle.js
+++ js/particle.js
@@ -76,16 +76,17 @@ var Particle = (function () {
 		checkCollision(this);
 
 		// gravity
-		for (var i = 0, max = Universe.particles.length; i < max; i += 1) {
-			if (Universe.particles[i] !== this) {
-				var distance = Util.distance(Universe.particles[i].x, this.x, Universe.particles[i].y, this.y);
-				var force = (this.mass * Universe.particles[i].mass) / Math.pow(distance, 2);
-				var speed = force / this.mass;
-				var angle = Math.atan2(Universe.particles[i].y - this.y, Universe.particles[i].x - this.x) + (Math.PI / 2);
+		var self = this;
+		Universe.particles.forEach(function (particle) {
+			if (particle !== self) {
+				var distance = Util.distance(particle.x, self.x, particle.y, self.y);
+				var force = (self.mass * particle.mass) / Math.pow(distance, 2);
+				var speed = force / self.mass;
+				var angle = Math.atan2(particle.y - self.y, particle.x - self.x) + (Math.PI / 2);
 
-				addVelocity(this, speed, angle);
+				addVelocity(self, speed, angle);
 			}
-		}
+		});
 	};
 
 	Particle.prototype.newMass = function (mass) {
